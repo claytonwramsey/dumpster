13f3441e979817ff0cebec232249bde6d58280a5
Théo Degioanni <theo.degioanni.llvm.deluge062@simplelogin.fr>
2024-06-02 15:20:47 +0100

    Implement collectable for TypeId
    This commit implements Collectable trivially for std::any::TypeId.
    
    In addition to GPLv3, I additionally license this individual contribution under the Apache 2.0 license, the MIT license, and the Mozilla Public License 2.0, if this is ever needed.

--- dumpster/src/impls.rs
+++ dumpster/src/impls.rs
@@ -21,6 +21,7 @@
 #![allow(deprecated)]
 
 use std::{
+    any::TypeId,
     borrow::Cow,
     cell::{Cell, OnceCell, RefCell},
     collections::{
@@ -280,6 +281,8 @@ collectable_trivial_impl!(DefaultHasher);
 collectable_trivial_impl!(RandomState);
 collectable_trivial_impl!(Rc<str>);
 collectable_trivial_impl!(SipHasher);
+
+collectable_trivial_impl!(TypeId);
 
 /// Implement [`Collectable`] for a tuple.
 macro_rules! collectable_tuple {
